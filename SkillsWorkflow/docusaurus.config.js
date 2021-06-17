274a506d93152ab69aab18375997b5a2145b0e9f
Afonso Cardoso <44061805+afonsogcardoso@users.noreply.github.com>
2021-06-17 17:32:58 +0100

    Update docusaurus.config.js

--- SkillsWorkflow/docusaurus.config.js
+++ SkillsWorkflow/docusaurus.config.js
@@ -27,7 +27,7 @@ module.exports = {
       siteId: 'd017bb9a-2fd6-4648-8b11-d67021355c27',
       branch: 'master',
       selector: 'div#search',
-      
+      indexName: 'netlify_d017bb9a-2fd6-4648-8b11-d67021355c27_master_all',
       // Optional: see doc section bellow
       contextualSearch: true
 
