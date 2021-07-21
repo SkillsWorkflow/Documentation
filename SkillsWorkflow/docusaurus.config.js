c071ec989f8a7c21caf4c9a701ac3547346f9c62
afonsogcardoso <afonso@skillsworkflow.com>
2021-07-21 16:29:31 +0100

    Removed contextual search

--- SkillsWorkflow/docusaurus.config.js
+++ SkillsWorkflow/docusaurus.config.js
@@ -32,7 +32,7 @@ module.exports = {
       selector: 'div#search',
       indexName: 'netlify_d017bb9a-2fd6-4648-8b11-d67021355c27_master_all',
       // Optional: see doc section bellow
-      contextualSearch: true,
+      //contextualSearch: true,
 
       // Optional: Algolia search parameters
       // searchParameters: {},
