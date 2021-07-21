9e2d12a430d8b01a14cdd2741c57f22717e56000
afonsogcardoso <afonso@skillsworkflow.com>
2021-07-21 09:53:50 +0100

    Fix algolia

--- SkillsWorkflow/docusaurus.config.js
+++ SkillsWorkflow/docusaurus.config.js
@@ -23,7 +23,7 @@ module.exports = {
   themeConfig: {
     algolia: {
       apiKey: 'dd45d8034a589d99ca6d342b4b8bc9cd',
-      //appId: 'KB6UV635OH',
+      appId: 'KB6UV635OH',
       siteId: 'd017bb9a-2fd6-4648-8b11-d67021355c27',
       branch: 'master',
       selector: 'div#search',
