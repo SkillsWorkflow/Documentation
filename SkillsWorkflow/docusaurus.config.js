02b000b7e93d78026d63d252915db19689ca6c43
afonsogcardoso <afonso@skillsworkflow.com>
2021-07-21 16:53:30 +0100

    added facetfilters

--- SkillsWorkflow/docusaurus.config.js
+++ SkillsWorkflow/docusaurus.config.js
@@ -37,7 +37,7 @@ module.exports = {
 
       // Optional: Algolia search parameters
       searchParameters: {
-        facetFilters: ["language:LANGUAGE"],
+        facetFilters: ["lang:en", "lang:pt"],
       },
 
       //... other Algolia params
