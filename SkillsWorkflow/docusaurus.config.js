a091451f1c17c05487b934c8ae5b77b32f5b48f1
Afonso Cardoso <afonsogcardoso@gmail.com>
2022-07-25 20:54:13 +0100

    Added Selector

--- SkillsWorkflow/docusaurus.config.js
+++ SkillsWorkflow/docusaurus.config.js
@@ -141,7 +141,8 @@ module.exports = {
       apiKey: "87094cef7d341d5684d9762da858c498",
       indexName: "sw_documentation",
       appId: "XB27C2B9IL",
-      contextualSeach: true
+      contextualSeach: true,
+      selector: 'div#'
     }
   },
   presets: [
