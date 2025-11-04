d433fd1cc52030e30b316eaebae18039de381911
Afonso Cardoso <44061805+afonsogcardoso@users.noreply.github.com>
2025-11-04 13:41:33 +0000

    added client redirects

--- SkillsWorkflow/docusaurus.config.cjs
+++ SkillsWorkflow/docusaurus.config.cjs
@@ -1,6 +1,5 @@
-import { themes as prismThemes } from 'prism-react-renderer';
-import { createRequire } from 'node:module';
-const require = createRequire(import.meta.url);
+// docusaurus.config.cjs
+const { themes: prismThemes } = require('prism-react-renderer');
 
 module.exports = {
   title: 'Skills Workflow’s Documentation',
@@ -12,160 +11,119 @@ module.exports = {
   favicon: 'img/favicon.ico',
   organizationName: 'SkillsWorkflow',
   projectName: 'Documentation',
+
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es', 'pt', 'pt-br']
   },
+
   themeConfig: {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula
     },
-    colorMode: {
-      respectPrefersColorScheme: true
-    },
-    docs: {
-      sidebar: {
-        hideable: true
-      }
-    },
+    colorMode: { respectPrefersColorScheme: true },
+    docs: { sidebar: { hideable: true } },
+
     navbar: {
       title: '',
-      logo: {
-        alt: 'Skills Workflow',
-        src: 'img/logo-blue.png'
-      },
+      logo: { alt: 'Skills Workflow', src: 'img/logo-blue.png' },
       items: [
+        { to: 'docs', label: 'Docs', position: 'left', className: 'navbar-item' },
+        { to: 'docs/university', label: 'University', position: 'left', className: 'navbar-item' },
+        { to: 'docs/trust', label: 'Trust', position: 'right', className: 'navbar-item' },
         {
-          to: 'docs',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
-          className: 'navbar-item'
-        },
-        {
-          to: 'docs/university',
-          label: 'University',
-          position: 'left',
-          className: 'navbar-item'
-        },
-        {
-          to: 'docs/trust',
-          label: 'Trust',
-          position: 'right',
-          className: 'navbar-item'
-        },
-        {
-          label: "API",
+          label: 'API',
           position: 'right',
           className: 'navbar-item',
           to: 'https://apiv2-demo-prod-we.skillsworkflow.com/swagger'
         },
-        {
-          type: 'localeDropdown',
-          position: 'right'
-        }
+        { type: 'localeDropdown', position: 'right' }
       ]
     },
+
     footer: {
       links: [
         {
           title: 'Customization',
           items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/documenting/style-guide',
-            },
-            {
-              label: 'Automations',
-              to: 'docs/customization/Automations'
-            },
-            {
-              label: 'SDK',
-              to: 'docs/sdk'
-            },
-            {
-              label: 'API',
-              to: 'docs/api/client-api'
-            },
-            {
-              label: 'Integrations',
-              to: 'docs/integrations/hr-link'
-            },
-            {
-              label: 'University',
-              to: 'docs/university'
-            }
-          ],
+            { label: 'Style Guide', to: 'docs/documenting/style-guide' },
+            { label: 'Automations', to: 'docs/customization/Automations' },
+            { label: 'SDK', to: 'docs/sdk' },
+            { label: 'API', to: 'docs/api/client-api' },
+            { label: 'Integrations', to: 'docs/integrations/hr-link' },
+            { label: 'University', to: 'docs/university' }
+          ]
         },
         {
           title: 'Learn More',
           items: [
-            {
-              label: 'Client API',
-              to: 'https://apiv2-demo-prod-we.skillsworkflow.com/swagger'
-            },
-            {
-              label: 'Integration API',
-              to: 'https://integration-api-test.skillsworkflow.com',
-            },
-            {
-              label: 'Website',
-              to: 'https://www.skillsworkflow.com/'
-            },
-            {
-              label: 'Status',
-              to: 'https://status.skillsworkflow.com/'
-            }
-          ],
+            { label: 'Client API', to: 'https://apiv2-demo-prod-we.skillsworkflow.com/swagger' },
+            { label: 'Integration API', to: 'https://integration-api-test.skillsworkflow.com' },
+            { label: 'Website', to: 'https://www.skillsworkflow.com/' },
+            { label: 'Status', to: 'https://status.skillsworkflow.com/' }
+          ]
         },
         {
           title: 'Social Media',
           items: [
-            {
-              label: 'Youtube',
-              to: 'https://www.youtube.com/channel/UCauqHRlHUSkS1H8KUYm4CiA'
-            },
-            {
-              label: 'Twitter',
-              to: 'https://twitter.com/skillsworkflow',
-            },
+            { label: 'Youtube', to: 'https://www.youtube.com/channel/UCauqHRlHUSkS1H8KUYm4CiA' },
+            { label: 'Twitter', to: 'https://twitter.com/skillsworkflow' },
             {
               label: 'LinkedIn',
               to: 'https://www.linkedin.com/company/skills-workflow/?viewAsMember=true'
             },
-            {
-              label: 'Instagram',
-              to: 'https://www.instagram.com/skillsworkflow'
-            },
-            {
-              label: 'Facebook',
-              to: 'https://www.facebook.com/skillsworkflow'
-            }
+            { label: 'Instagram', to: 'https://www.instagram.com/skillsworkflow' },
+            { label: 'Facebook', to: 'https://www.facebook.com/skillsworkflow' }
           ]
         }
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Skills Workflow`,
+      copyright: `Copyright © ${new Date().getFullYear()} Skills Workflow`
     },
+
+    // Algolia (DocSearch hospedado)
     algolia: {
-      apiKey: "87094cef7d341d5684d9762da858c498",
-      indexName: "sw_documentation",
-      appId: "XB27C2B9IL",
+      appId: 'XB27C2B9IL',
+      apiKey: '87094cef7d341d5684d9762da858c498', // chave pública
+      indexName: 'sw_documentation',
       contextualSearch: true,
-      selector: 'div#'
+      searchPagePath: 'search'
     }
   },
+
   presets: [
     [
       '@docusaurus/preset-classic',
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.cjs'),
-          editUrl: 'https://github.com/SkillsWorkflow/Documentation/edit/master/SkillsWorkflow',
+          editUrl:
+            'https://github.com/SkillsWorkflow/Documentation/edit/master/SkillsWorkflow',
           editLocalizedFiles: true
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve('./src/css/custom.css')
+        },
+        // sitemap configurado aqui, não no array plugins
+        sitemap: {
+          changefreq: 'daily',
+          priority: 0.5,
+          ignorePatterns: ['/tags/**', '/search'],
+          filename: 'sitemap.xml'
+        }
+      }
+    ]
+  ],
+
+  plugins: [
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        createRedirects(existingPath) {
+          if (existingPath.endsWith('/index')) {
+            return [existingPath.replace(/\/index$/, '')];
+          }
+          return [];
         }
       }
     ]
